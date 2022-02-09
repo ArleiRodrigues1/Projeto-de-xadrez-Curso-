ff9dda93712fb1e2fb1442a4d9eda8ac165d4c3a
ArleiRodrigues1 <Arlei.freire011@gmail.com>
2022-02-09 17:31:32 -0300

    Jogada especial promocao

--- Xadrez.Console/xadrez/PartidaDeXadrez.cs
+++ Xadrez.Console/xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,23 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+           
+            Peca p = tab.peca(destino);
+
+            // #Jogadaespcial - Promocao
+            if(p is Peao)
+            {
+                if(p.cor == Cor.Branca && destino.linha == 0 || destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -158,7 +175,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            Peca peca = tab.peca(destino);
 
             //#JogadaEspecial - En Passant
             if(p is Peao && destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)
